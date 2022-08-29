2af5b83a8937e5d40bea0284ed23f3c668ab8a33
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2022-08-30 01:04:45 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import './App.css';
+import { Route } from 'react-router-dom';
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
 
-function App() {
-  const name = 'REACT';
-  return <div className="react">{name}</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Route component={PostListPage} path={['/@:username', '/']} exact />
+      <Route component={LoginPage} path="/login" />
+      <Route component={RegisterPage} path="/register" />
+      <Route component={WritePage} path="/write" />
+      <Route component={PostPage} path="/@:username/:postID" />
+    </>
+  );
+};
 export default App;
-<test>
-<test>
