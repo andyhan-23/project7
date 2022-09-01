eb45f92be0a6cfbe40f845ec6e52d3ab811184db
andyhan-23 <98483125+andyhan-23@users.noreply.github.com>
2022-09-01 09:03:12 +0900

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*
 import { Route } from 'react-router-dom';
 import PostListPage from './pages/PostListPage';
 import LoginPage from './pages/LoginPage';
@@ -8,12 +9,50 @@ import PostPage from './pages/PostPage';
 const App = () => {
   return (
     <>
-      <Route component={PostListPage} path={['/@:username', '/']} exact />
-      <Route component={LoginPage} path="/login" />
-      <Route component={RegisterPage} path="/register" />
+    
+      <Route component={PostListPage} path="/@:username" exact />
+      <Route component={PostListPage} path="/" exact />
+      <Route component={LoginPage} path= "/login"  />
+      <Route component={RegisterPage} path= "/register"  />
       <Route component={WritePage} path="/write" />
-      <Route component={PostPage} path="/@:username/:postID" />
+      <Route component={PostPage} path="/@:username/:postid" />
+      
     </>
   );
 };
+
+export default App;
+
+*/
+import React from "react";
+import PostListPage from './pages/PostListPage';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+import WritePage from './pages/WritePage';
+import PostPage from './pages/PostPage';
+import "./App.css";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+
+
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className="App">
+        
+      <Routes>
+          <Route path="/" exact element={<PostListPage />} />
+          <Route path="/login" exact element={<LoginPage  />} />
+          <Route path="/register" exact element={<RegisterPage/>} />
+          <Route path="/write" exact element={<WritePage/>} />
+          <Route path="/post" exact element={<PostPage/>} />
+      </Routes>
+      
+      </div>
+    </BrowserRouter>
+  );
+}
+
 export default App;
